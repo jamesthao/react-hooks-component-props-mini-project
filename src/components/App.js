b70bda3e32433f98dfd01dfbd1433603d9c38261
James <jamesthao32@gmail.com>
2022-06-02 20:27:02 -0600

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import blogData from "../data/blog";
+import About from "./About";
+import ArticleList from "./ArticleList";
+import Header from "./Header";
 
 console.log(blogData);
 
@@ -8,8 +11,10 @@ function App() {
     <div className="App">
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
+      <Header blogName={blogData.name} />
+      <About blogImage={blogData.image} blogAbout={blogData.about} />
+      <ArticleList blogInfo={blogData.posts}/>
     </div>
   );
 }
-
 export default App;
